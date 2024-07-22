3cb94098ad5eba940c8c537504639668c74c09d0
ToastedToast <hey@toasted.dev>
2024-07-22 18:19:08 +0800

    fix: lint errors

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -1,16 +1,16 @@
-import {nextui} from '@nextui-org/react'
+import {nextui} from '@nextui-org/react';
+import type {Config} from 'tailwindcss';
 
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+    '../node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {},
   },
   darkMode: "class",
   plugins: [nextui()],
-}
+} satisfies Config
